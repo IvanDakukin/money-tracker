e63b9a754555a659079f9ffdbf699befb7f5ff5d
Ivan Dakukin <ivdakukin@gmail.com>
2024-04-06 07:35:15 +0300

    Обновил версию vue router

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import MainPage from "../views/MainPage.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  }
-]
+    path: "/",
+    name: "home",
+    component: MainPage,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
