c40c0826e4fa1e7702352a4b49f42a8dc0d16106
Ivan Dakukin <ivdakukin@gmail.com>
2024-04-07 04:05:06 +0300

    Добавил функционал добавления статей в стор

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,36 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    transactions: [
+      {
+        id: 1,
+        date: new Date(),
+        sum: 40000,
+        title: "Зарплата",
+      },
+    ],
+  },
+  getters: {
+    nextId(state) {
+      return state.transactions.length + 1;
+    },
+    transactionsForDay: (state) => (date) =>
+      state.transactions.filter(
+        (transaction) =>
+          transaction.date.getFullYear() === date.getFullYear() &&
+          transaction.date.getMonth() === date.getMonth() &&
+          transaction.date.getDate() == date.getDate()
+      ),
+  },
+  mutations: {
+    addTransaction(state, transaction) {
+      state.transactions.push(transaction);
+    },
+  },
+  actions: {
+    addTransaction(context, transaction) {
+      context.commit("addTransaction", transaction);
+    },
+  },
 });
